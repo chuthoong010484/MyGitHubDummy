1728bdac4abc5fef6c130afb8729d20ea379dc45
chuthoong010484 <chuthoong010484@yahoo.com>
2016-05-07 10:53:28 +0700

    commit 2

--- MyFirstGitHub/Program.cs
+++ MyFirstGitHub/Program.cs
@@ -11,6 +11,7 @@ namespace MyFirstGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world !");
+            Console.WriteLine("Hello world 2 !");
         }
     }
 }
